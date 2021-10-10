1de2ba194d6d76e5880e3be1daf1762dace49d18
Alexandr_P <apalion97@gmail.com>
2021-10-10 19:00:51 +0300

    Global Scope and Functions

--- script.js
+++ script.js
@@ -162,3 +162,24 @@ function functionWithArgs(a, b) {
     console.log(a + b);
 }
 functionWithArgs(2, 2)
+
+//Global Scope and Functions
+let myGlobal = 10;
+
+function fun1() {
+    // Assign 5 to oopsGlobal Here
+    oopsGlobal = 5;
+}
+
+function fun2() {
+    let output = "";
+    if (typeof myGlobal != "undefined") {
+        output += "myGlobal: " + myGlobal;
+    }
+    if (typeof oopsGlobal != "undefined") {
+        output += " oopsGlobal: " + oopsGlobal;
+    }
+    console.log(output);
+}
+
+//
