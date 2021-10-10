e4bd7812dd94e01122db86823d4da70508fae911
Alexandr_P <apalion97@gmail.com>
2021-10-10 18:45:12 +0300

    Access Multi-Dimensional Arrays With Indexes

--- script.js
+++ script.js
@@ -107,17 +107,22 @@ var myAdverb = "quickly";
 var wordBlanks = `${myNoun} ${myAdjective} ${myVerb} ${myAdverb}`; // Change this line
 
 //Store Multiple Values in one Variable using JavaScript Arrays
-var myArray = ['sdfs', 123];
+// var myArray = ['sdfs', 123];
 
 //Nest one Array within Another Array
 var myArray2 = [["Bulls", 23], ["White Sox", 45]];
 
 //Access Array Data with Indexes
-var myArray3 = [50,60,70];
-let myData = myArray3[0];
+// var myArray3 = [50,60,70];
+// let myData = myArray3[0];
 
 //Modify Array Data With Indexes
-var myArray4 = [18,64,99];
-myArray4[0] = 45;
+// var myArray4 = [18,64,99];
+// myArray4[0] = 45;
+
+//Access Multi-Dimensional Arrays With Indexes
+var myArray = [[1,2,3], [4,5,6], [7,8,9], [[10,11,12], 13, 14]];
+
+var myData = myArray[2][1];
 
 //
