7a9715addbb4e9afeff0d4f49c13d929a8f0c5b9
Alexandr_P <apalion97@gmail.com>
2021-10-10 16:14:16 +0300

    Compound Assignment With Augmented Subtraction

--- script.js
+++ script.js
@@ -2,13 +2,23 @@
 
 //Compound Assignment With Augmented Addition
 
-var a = 3;
-var b = 17;
-var c = 12;
+// var a = 3;
+// var b = 17;
+// var c = 12;
+//
+//
+// a +=12;
+// b +=9;
+// c +=7;
 
+//Compound Assignment With Augmented Subtraction
 
-a +=12;
-b +=9;
-c +=7;
+var a = 11;
+var b = 9;
+var c = 3;
+
+a -= 6;
+b -= 15;
+c -= 1;
 
 //
