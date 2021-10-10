049175a2feffed42d844906b1200b746243d7301
Alexandr_P <apalion97@gmail.com>
2021-10-10 16:20:51 +0300

    Declare String Variables

--- script.js
+++ script.js
@@ -33,12 +33,16 @@
 
 //Compound Assignment With Augmented Division
 
-var a = 48;
-var b = 108;
-var c = 33;
+// var a = 48;
+// var b = 108;
+// var c = 33;
+//
+// a /= 12;
+// b /= 4;
+// c /= 11;
 
-a /= 12;
-b /= 4;
-c /= 11;
+//Declare String Variables
+let myFirstName = 'Alex';
+let myLastName = 'Petrovich';
 
 //
